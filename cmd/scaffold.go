9f22a226762b00274337130c68692061723710f0
Damian ONeill <damian.oneill@me.com>
2019-11-05 16:38:34 +0000

    refactor common scaffold code

--- cmd/scaffold.go
+++ cmd/scaffold.go
@@ -1,7 +1,3 @@
-/*
-Copyright © 2019 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
@@ -44,6 +40,37 @@ var scaffoldCmd = &cobra.Command{
 	},
 }
 
+func collectInfo(config Config, path, folder, resource, message string) *resty.Response {
+	os.Mkdir(path+string(filepath.Separator)+folder, os.ModePerm)
+
+	resp, err := GET(config.Resource, resource, config.Username, config.Password)
+	if err != nil {
+		fmt.Printf(message+" %v", err)
+		os.Exit(1)
+	}
+	switch resp.StatusCode() {
+	case 200:
+		break
+	default:
+		fmt.Printf(message+": %v \n", resp.String())
+		os.Exit(1)
+	}
+	return resp
+}
+
+func writeInfo(config interface{}, path, folder, filename string) {
+	data, err := yaml.Marshal(config)
+	if err != nil {
+		fmt.Printf("Problem with Marshalling Yaml: %v", err)
+		os.Exit(1)
+	}
+	err = ioutil.WriteFile(path+string(filepath.Separator)+folder+string(filepath.Separator)+filename, data, os.ModePerm)
+	if err != nil {
+		fmt.Printf("Problem writing "+folder+" config %v", err)
+		os.Exit(1)
+	}
+}
+
 func scaffold(config Config, path string) {
 	fmt.Printf("Healthbot scaffold: %v\n", config.Resource)
 	if _, err := os.Stat(path); os.IsNotExist(err) {
@@ -57,20 +84,7 @@ func scaffold(config Config, path string) {
 
 	//
 
-	os.Mkdir(path+string(filepath.Separator)+"devices", os.ModePerm)
-
-	resp, err := GET(config.Resource, "/api/v1/devices/", config.Username, config.Password)
-	if err != nil {
-		fmt.Printf("Problem getting Devices %v", err)
-		return
-	}
-	switch resp.StatusCode() {
-	case 200:
-		break
-	default:
-		fmt.Printf("Problem getting Devices: %v \n", resp.String())
-		return
-	}
+	resp := collectInfo(config, path, "devices", "/api/v1/devices/", "Problem getting Devices")
 
 	var devices types.Devices
 	if err := json.Unmarshal(resp.Body(), &devices); err != nil {
@@ -86,84 +100,31 @@ func scaffold(config Config, path string) {
 		}
 	}
 
-	data, err := yaml.Marshal(&devices)
-	if err != nil {
-		fmt.Printf("Problem with Marshalling Yaml: %v", err)
-		return
-	}
-	err = ioutil.WriteFile(path+string(filepath.Separator)+"devices"+string(filepath.Separator)+"devices.yml", data, os.ModePerm)
-	if err != nil {
-		fmt.Printf("Problem writing Devices Config %v", err)
-		return
-	}
+	writeInfo(devices, path, "devices", "devices.yml")
 
 	//
 
-	os.Mkdir(path+string(filepath.Separator)+"device-groups", os.ModePerm)
-
-	resp, err = GET(config.Resource, "/api/v1/device-groups/", config.Username, config.Password)
-	if err != nil {
-		fmt.Printf("Problem getting Device Groups %v", err)
-		return
-	}
-	switch resp.StatusCode() {
-	case 200:
-		break
-	default:
-		fmt.Printf("Problem getting Device Groups: %v \n", resp.String())
-		return
-	}
+	dgResp := collectInfo(config, path, "device-groups", "/api/v1/device-groups/", "Problem getting Devices Groups")
 
 	var deviceGroups types.DeviceGroups
-	if err := json.Unmarshal(resp.Body(), &deviceGroups); err != nil {
+	if err := json.Unmarshal(dgResp.Body(), &deviceGroups); err != nil {
 		fmt.Printf("%v", err)
 		return
 	}
 
-	data, err = yaml.Marshal(&deviceGroups)
-	if err != nil {
-		fmt.Printf("Problem with Marshalling Yaml: %v", err)
-		return
-	}
-	err = ioutil.WriteFile(path+string(filepath.Separator)+"device-groups"+string(filepath.Separator)+"device-groups.yml", data, os.ModePerm)
-	if err != nil {
-		fmt.Printf("Problem writing Device Groups Config %v", err)
-		return
-	}
+	writeInfo(deviceGroups, path, "device-groups", "device-groups.yml")
 
 	//
 
 	os.Mkdir(path+string(filepath.Separator)+"playbook-instances", os.ModePerm)
 
-	resp, err = GET(config.Resource, "/api/v1/device-groups/", config.Username, config.Password)
-	if err != nil {
-		fmt.Printf("Problem getting Device Groups %v", err)
-		return
-	}
-	switch resp.StatusCode() {
-	case 200:
-		break
-	default:
-		fmt.Printf("Problem getting Device Groups: %v \n", resp.String())
-		return
-	}
-
 	var playbookInstances types.PlaybookInstances
-	if err := json.Unmarshal(resp.Body(), &playbookInstances); err != nil {
+	if err := json.Unmarshal(dgResp.Body(), &playbookInstances); err != nil {
 		fmt.Printf("%v", err)
 		return
 	}
 
-	data, err = yaml.Marshal(&playbookInstances)
-	if err != nil {
-		fmt.Printf("Problem with Marshalling Yaml: %v", err)
-		return
-	}
-	err = ioutil.WriteFile(path+string(filepath.Separator)+"playbook-instances"+string(filepath.Separator)+"playbook-instances.yml", data, os.ModePerm)
-	if err != nil {
-		fmt.Printf("Problem writing Playbook Instances Config %v", err)
-		return
-	}
+	writeInfo(playbookInstances, path, "playbook-instances", "playbook-instances.yml")
 
 }
 
